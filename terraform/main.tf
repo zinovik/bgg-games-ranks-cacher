d5d61ad9e3c59cee68fff1313653ed78922ae8ed
Max Zinovik <zinovik@gmail.com>
2023-12-26 12:56:21 +0100

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+    project = "zinovik-project"
+    region  = "us-central1"
+}
+
+resource "google_service_account" "github-actions" {
+    account_id = "github-actions"
+}
+
+resource "google_storage_bucket" "static" {
+    name          = "boardgamegeek2"
+    location      = "us-central1"
+
+    cors {
+        origin          = ["https://zinovik.github.io", "http://localhost:3000"]
+        method          = ["GET"]
+        response_header = ["Content-Type"]
+        max_age_seconds = 3600
+    }
+
+    versioning {
+        enabled = true
+    }
+}
