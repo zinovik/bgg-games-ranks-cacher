456de7a09db11101aaeb3409ed9ece10d01f4f8b
Max Zinovik <zinovik@gmail.com>
2024-01-01 17:09:50 +0100

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import { Main } from './main/Main';
 import { BGGGamesRanksService } from './data/BGGGamesRanks.service';
 import { GoogleStorageService } from './storage/GoogleStorage.service';
 
+const BUCKET_NAME = 'boardgamegeek';
+const FILE_NAME = 'bgg-games-ranks-top1000-latest.json';
+const PARSER_URL =
+    'https://us-central1-zinovik-project.cloudfunctions.net/bgg-games-ranks-parser';
+
 functions.http('main', async (_req, res) => {
     console.log('Triggered!');
 
     const main = new Main(
-        new BGGGamesRanksService(),
-        new GoogleStorageService()
+        new BGGGamesRanksService(PARSER_URL),
+        new GoogleStorageService(BUCKET_NAME, FILE_NAME)
     );
 
     await main.sendMessage();
--- src/storage/GoogleStorage.service.ts
+++ src/storage/GoogleStorage.service.ts
@@ -2,19 +2,19 @@ import { Bucket, Storage, File } from '@google-cloud/storage';
 import { StorageService } from './Storage.interface';
 import { Data } from '../common/model/Data.interface';
 
-const BUCKET_NAME = 'boardgamegeek';
-const FILE_NAME = 'bgg-games-ranks-top1000-latest.json';
-
 export class GoogleStorageService implements StorageService {
-    private readonly storage: Storage = new Storage();
     private readonly bucket: Bucket;
 
-    constructor() {
-        this.bucket = this.storage.bucket(BUCKET_NAME);
+    constructor(
+        private readonly bucketName: string,
+        private readonly fileName: string
+    ) {
+        const storage: Storage = new Storage();
+        this.bucket = storage.bucket(this.bucketName);
     }
 
     async setData(data: Data): Promise<void> {
-        const file: File = this.bucket.file(FILE_NAME);
+        const file: File = this.bucket.file(this.fileName);
         const dataBuffer = Buffer.from(JSON.stringify(data));
         await file.save(dataBuffer, {
             gzip: true,
